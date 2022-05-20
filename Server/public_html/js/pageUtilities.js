c2091e88a62b44fc563cb43102a5511d177f55af
SLMcCarthy <slmccarthy@loyola.edu>
2022-05-20 16:37:32 -0400

    Bit of refactoring with page utilities JS file

--- Server/public_html/js/pageUtilities.js
+++ Server/public_html/js/pageUtilities.js
@@ -1,12 +1,17 @@
-/* Page Utilities!
-*  Used to store simple functions used across most pages to avoid re-defining them in the pages scripts
-*/
+// Page Utilities
+// Used to store simple functions used across most pages to avoid re-defining them in each pages script
 
-// Convert Listing dates to more readable strings
-const dateString = function(dateObj){ return new Date(dateObj).toDateString(); }
+// Convert Date to string format: Weekday Month Day Year (Eg: Mon Apr 11 2022)
+function dateString(dateObj){ return new Date(dateObj).toDateString(); }
+
+// Convert Date to string format: MM/DD/YYYY, Localtime (Eg: 4/11/2022, 8:30:00 PM)
+function dateStringLocale(dateObj){ return new Date(dateObj).toLocaleString(); }
 
 // Confirm with user when logging out
-const confirmLogout = function(){ return window.confirm('Are you sure you want to log out?'); };
+function confirmLogout(){ return window.confirm('Are you sure you want to log out?'); }
 
-// Confirm with user when following expanded listing links
-const confirmFollowLink = function() { return window.confirm('Are you sure you want to leave this page?'); };
+// Confirm with user when following external links (mainly links in expanded listings)
+function confirmFollowLink(){ return window.confirm('Are you sure you want to leave this page?'); }
+
+// Confirm with admin when deleting a user
+function confirmDeleteUser(){ return window.confirm('Are you sure you want to delete this user?'); }
